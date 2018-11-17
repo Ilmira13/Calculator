c50623322c2577cd55857a8e7662fe4a22c541c0
Ilmira13 <ilmira13121993@yandex.ru>
2018-11-17 17:55:55 +0500

    not finalized

---
+++ Unit1.dfm
@@ -0,0 +1,192 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Calculator'
+  ClientHeight = 432
+  ClientWidth = 718
+  Color = clActiveCaption
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  ShowHint = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Edit1: TEdit
+    Left = 192
+    Top = 152
+    Width = 237
+    Height = 21
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 240
+    Top = 271
+    Width = 75
+    Height = 25
+    Caption = '+'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 304
+    Top = 302
+    Width = 75
+    Height = 25
+    Caption = '-'
+    TabOrder = 2
+  end
+  object Button3: TButton
+    Left = 354
+    Top = 333
+    Width = 75
+    Height = 25
+    Caption = '/'
+    TabOrder = 3
+  end
+  object Button4: TButton
+    Left = 192
+    Top = 240
+    Width = 75
+    Height = 25
+    Caption = '*'
+    TabOrder = 4
+  end
+  object Button5: TButton
+    Left = 192
+    Top = 333
+    Width = 75
+    Height = 25
+    Caption = '='
+    TabOrder = 5
+    OnClick = Button5Click
+  end
+  object Button6: TButton
+    Left = 354
+    Top = 240
+    Width = 75
+    Height = 25
+    Caption = 'Clear all'
+    TabOrder = 6
+    OnClick = Button6Click
+  end
+  object Button7: TButton
+    Left = 192
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = '0'
+    TabOrder = 7
+    OnClick = Button7Click
+  end
+  object Button8: TButton
+    Left = 273
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = '1'
+    TabOrder = 8
+    OnClick = Button8Click
+  end
+  object Button9: TButton
+    Left = 354
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = '2'
+    TabOrder = 9
+    OnClick = Button9Click
+  end
+  object Button10: TButton
+    Left = 192
+    Top = 87
+    Width = 75
+    Height = 25
+    Caption = '3'
+    TabOrder = 10
+    OnClick = Button10Click
+  end
+  object Button11: TButton
+    Left = 273
+    Top = 87
+    Width = 75
+    Height = 25
+    Caption = '4'
+    TabOrder = 11
+    OnClick = Button11Click
+  end
+  object Button12: TButton
+    Left = 354
+    Top = 87
+    Width = 75
+    Height = 25
+    Caption = '5'
+    TabOrder = 12
+    OnClick = Button12Click
+  end
+  object Button13: TButton
+    Left = 192
+    Top = 118
+    Width = 75
+    Height = 25
+    Caption = '6'
+    TabOrder = 13
+    OnClick = Button13Click
+  end
+  object Button14: TButton
+    Left = 273
+    Top = 118
+    Width = 75
+    Height = 25
+    Caption = '7'
+    TabOrder = 14
+    OnClick = Button14Click
+  end
+  object Button15: TButton
+    Left = 354
+    Top = 118
+    Width = 75
+    Height = 25
+    Caption = '8'
+    TabOrder = 15
+    OnClick = Button15Click
+  end
+  object Button16: TButton
+    Left = 256
+    Top = 25
+    Width = 75
+    Height = 25
+    Caption = '9'
+    TabOrder = 16
+    OnClick = Button16Click
+  end
+  object Button17: TButton
+    Left = 488
+    Top = 87
+    Width = 75
+    Height = 25
+    Caption = '0'
+    TabOrder = 17
+    OnClick = Button17Click
+  end
+  object Button18: TButton
+    Left = 488
+    Top = 184
+    Width = 75
+    Height = 25
+    Caption = '.'
+    TabOrder = 18
+    OnClick = Button18Click
+  end
+  object Button19: TButton
+    Left = 496
+    Top = 280
+    Width = 75
+    Height = 25
+    Caption = '<-'
+    TabOrder = 19
+    OnClick = Button19Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,179 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button18: TButton;
+    Button19: TButton;
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button9Click(Sender: TObject);
+    procedure Button10Click(Sender: TObject);
+    procedure Button11Click(Sender: TObject);
+    procedure Button12Click(Sender: TObject);
+    procedure Button13Click(Sender: TObject);
+    procedure Button14Click(Sender: TObject);
+    procedure Button15Click(Sender: TObject);
+    procedure Button16Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button18Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button19Click(Sender: TObject);
+    procedure Button17Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  a, b: string;
+  f, t, tt: int64;
+implementation
+
+{$R *.dfm}
+
+ // Определение времени работы метода, секунды
+procedure CalculateAndShowMethodTime(f, t, tt: Int64);
+begin
+  // QueryPerformanceFrequency(f) - количество тактов процессора в секунду
+  // QueryPerformanceCounter показания(t) счётчиков такта перед выполннием метода
+  // QueryPerformanceCounter показания(tt) счётчиков такта после выполнения метода
+
+  // Расчёт времени и вывод на экран
+  ShowMessage('Время вычисления, с: ' + FloatToStr((tt - t) / f));
+end;
+
+
+procedure TForm1.Button10Click(Sender: TObject);
+begin
+  a := a + '3';
+  Edit1.Text := Edit1.Text + '3';
+end;
+
+procedure TForm1.Button11Click(Sender: TObject);
+begin
+  a := a + '4';
+  Edit1.Text := Edit1.Text + '4';
+end;
+
+procedure TForm1.Button12Click(Sender: TObject);
+begin
+  a := a + '5';
+  Edit1.Text := Edit1.Text + '5';
+end;
+
+procedure TForm1.Button13Click(Sender: TObject);
+begin
+  a := a + '6';
+  Edit1.Text := Edit1.Text + '6';
+end;
+
+procedure TForm1.Button14Click(Sender: TObject);
+begin
+  a := a + '7';
+  Edit1.Text := Edit1.Text + '7';
+end;
+
+procedure TForm1.Button15Click(Sender: TObject);
+begin
+  a := a + '8';
+  Edit1.Text := Edit1.Text + '8';
+end;
+
+procedure TForm1.Button16Click(Sender: TObject);
+begin
+  a := a + '9';
+  Edit1.Text := Edit1.Text + '9';
+end;
+
+procedure TForm1.Button17Click(Sender: TObject);
+begin
+  a := a + '0';
+  Edit1.Text := Edit1.Text + '0';
+end;
+
+procedure TForm1.Button18Click(Sender: TObject);
+begin
+  a := a + '.'; // точка или запятая, зависит от настроек в панели управления ->
+                // -> часы и регион -> региональные стандарты -> вкладка про числа
+  Edit1.Text := Edit1.Text + '.';
+end;
+
+procedure TForm1.Button19Click(Sender: TObject);
+var
+  s: string;
+  i: integer;
+begin
+  For i := 1 to Length(Edit1.Text) - 1 do
+      s := s + Edit1.Text[i];
+      Edit1.Text := s;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Edit1.Text := Edit1.Text + ' + ';
+  b := a;
+  a := '';
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+   Edit1.Text := Edit1.Text + ' = ' + FloatToStr(StrToFloat(a) + StrToFloat(b));
+end;
+
+
+procedure TForm1.Button6Click(Sender: TObject);
+begin
+  QueryPerformanceFrequency(f);
+  QueryPerformanceCounter(t);
+  Edit1.Text := '';
+  QueryPerformanceCounter(tt);
+  CalculateAndShowMethodTime(f, t, tt);
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+  a := a + '0';
+  Edit1.Text := Edit1.Text + '0';
+end;
+
+procedure TForm1.Button8Click(Sender: TObject);
+begin
+  a := a + '1';
+  Edit1.Text := Edit1.Text + '1';
+end;
+
+procedure TForm1.Button9Click(Sender: TObject);
+begin
+  a := a + '2';
+  Edit1.Text := Edit1.Text + '2';
+end;
+
+
+
+end.
